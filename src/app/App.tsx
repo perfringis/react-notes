5d8c907e6d241671ad44a6b36d717599596456d1
perfringis <kopko.mateusz@gmail.com>
2025-08-03 11:35:27 +0200

    write example for jsx syntax markup

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./App.css";
+import AboutProfile from "../components/about-profile/AboutProfile";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <AboutProfile/>
+  </div>;
 }
 
 export default App;
---
+++ src/components/about-profile/AboutProfile.tsx
@@ -0,0 +1,17 @@
+// React provides own markup syntax called JSX
+// it's similar to HTML
+
+function AboutProfile() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>
+        Hello there.
+        <br />
+        How do you do?
+      </p>
+    </>
+  );
+}
+
+export default AboutProfile;
